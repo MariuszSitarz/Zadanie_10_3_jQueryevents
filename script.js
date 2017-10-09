b9d48f30340c41bea9b30ae7bb27b5be988c7ee2
Mariusz Sitarz <mariusz_sitarz@wp.pl>
2017-10-09 16:44:08 +0200

    slider with issues

---
+++ script.js
@@ -0,0 +1,15 @@
+$(function () {
+    var carouselList = $("#carousel ul");
+
+    setInterval(function () {
+        carouselList.animate({'marginLeft': -400}, 700, moveFirstSlide);
+    }, 2000);
+
+
+    function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft: 0});
+    }
+});
